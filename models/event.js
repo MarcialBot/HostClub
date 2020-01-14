d742e225620276a9e5e8402e45c532020e04c8b8
Khris <khristianmarcial@gmail.com>
2020-01-13 20:49:26 -0500

    Adding database files and oauth config

--- models/event.js
+++ models/event.js
@@ -7,33 +7,6 @@ const eventSchema = new Schema({
     invites: [String]
 });
 
-const events = [
-    {event: `Party at Khris's House`, done: true},
-    {event: `Party at Khris's House`, done: true},
-    {event: `Party at Khris's House`, done: false},
-    {event: `Party at Khris's House`, done: true},
-]
+module.exports = mongoose.model('Event', eventSchema);
 
-module.exports = {
-    getAll,
-    getOne,
-    create,
-    deleteOne,
-    mongoose.model('Event', eventSchema)
-}
-
-function getOne(id) {
-    return events[id];
-}
-
-function getAll() {
-    return events;
-}
-
-function create(event) {
-    events.push(event);
-}
-
-function deleteOne(id) {
-    events.splice(id, 1);
-}
+//Replace events in all files with Event
